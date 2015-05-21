bdc85894c68c555259bedcf75686e92a84a902af
tux-mind <massimo.dragano@gmail.com>
2015-05-21 12:18:15 +0200

    setup AngularJS and angular-rails-templates in our asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require angular
+//= require angular-route
+//= require angular-rails-templates
+//= require_tree ../templates
 //= require_tree .
