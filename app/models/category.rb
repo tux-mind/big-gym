f62f112aea91ea1ec222b039f867e3b3bbeade4c
tux-mind <massimo.dragano@gmail.com>
2015-05-22 19:18:48 +0200

    add instructor relation and thumb method to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,13 @@
 class Category < ActiveRecord::Base
   has_many :Courses
+  has_many :Instructors, through: :Courses
   
   has_attached_file :image, :styles => {
     :medium => "300x300>",
     :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  
+  def thumb
+    image.url(:thumb)
+  end
 end
