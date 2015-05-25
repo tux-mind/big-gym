a2c2851707bc2377576ad9611dfba1f0edc9c953
tux_mind <massimo.dragano@gmail.com>
2015-05-26 00:05:00 +0200

    removed image methods from model

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,8 +6,4 @@ class Category < ActiveRecord::Base
     :medium => "300x300>",
     :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  
-  def thumb
-    image.url(:thumb)
-  end
 end
