71d22a008e774cb7c9ed413fa94c57e75137cd07
tux-mind <massimo.dragano@gmail.com>
2015-05-26 23:31:04 +0200

    use lambda expression that work on both ruby19 and ruby20

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,17 +1,16 @@
 
 
 class Testimonial < ActiveRecord::Base
+  
   has_attached_file :video,
       styles: {
         medium: {
             geometry: "300x300!",
             format: 'jpg',
-            time: -> (attachment) {
-                attachment.instance.method(:get_middle)}}},
+            time: lambda {|attachment|
+                    lambda {|meta, options|
+                      meta[:duration]/2}}}},
       processors: [ :transcoder ]
   validates_attachment_content_type :video, :content_type => /\Avideo\/.*\Z/
   
-  def get_middle(meta, options)
-    meta[:duration] / 2
-  end
 end
